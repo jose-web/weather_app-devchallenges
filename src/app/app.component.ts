7e4d23137e1a847b3fb826219e3bbbb2b7da44a7
jose <delossantosramosjose@gmail.com>
2021-12-02 20:40:43 +0100

    Added data visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,39 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'WeatherApp';
   
-  week:any = [
-    {
-      name: "Tomorrow",
-      img: "clear.png",
-      max: 16,
-      min: 11
-    },
-    {
-      name: "Tomorrow",
-      img: "clear.png",
-      max: 16,
-      min: 11
-    },  
-    {
-      name: "Tomorrow",
-      img: "clear.png",
-      max: 16,
-      min: 11
-    },  
-    {
-      name: "Tomorrow",
-      img: "clear.png",
-      max: 16,
-      min: 11
-    },  
-    {
-      name: "Tomorrow",
-      img: "clear.png",
-      max: 16,
-      min: 11
-    },
-  ]
+  week:any = []
+  location:string = ''
+
+  setLocation(weather: any){
+    this.week = weather.consolidated_weather
+    this.location = weather.parent.title + ", " + weather.title
+  }
+
+  getImage(weather_state_name:string){
+    return 'assets/'+ weather_state_name?.replace(" ","")+'.png'
+  }
+
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'menu',
+  selector: 'Menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.sass']
 })
 
 export class Menu {
+
+  @Output()
+  locationMenu = new EventEmitter<any>()
+
   searchForm:FormGroup = this.formBuilder.group({
     search: ''
-  });
+  })
 
   location:any = []
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    this.locationMenu.emit({hola:"saludo"})
+    this.sendLocation(766273)
+  }
 
   onSubmit(){
     let search = this.searchForm.value.search
@@ -23,4 +30,11 @@ export class Menu {
     .then(json =>json.json())
     .then(response => this.location = response)
   }
+
+  sendLocation(woeid:number){    
+    fetch('https://www.metaweather.com/api/location/'+woeid)
+    .then(json =>json.json())
+    .then(response => this.locationMenu.emit(response))
+  }
+
 }
